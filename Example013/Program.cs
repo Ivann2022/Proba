ee2853f814c53ce3b320772e8b5631fb22e67182
Ivan <117516230+Ivann2022@users.noreply.github.com>
2022-12-17 15:17:31 +0800

    взрыв мозга

--- Example013/Program.cs
+++ Example013/Program.cs
@@ -122,7 +122,7 @@ PrintImage(pic);
 
 
 
-int Factorial(int n)        
+double Factorial(int n)        
 {
     // 1! = 1
     // 0! = 1
@@ -138,3 +138,19 @@ for (int i = 1; i < 40; i++)
 
 
 
+
+
+// f(2) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
